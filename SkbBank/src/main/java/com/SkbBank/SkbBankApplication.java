713787e58b5d59061480b0d0668e7ea93c7b3984
khadar <khadar3099@gmail.com>
2025-02-20 00:28:34 +0530

    initial commit

---
+++ SkbBank/src/main/java/com/SkbBank/SkbBankApplication.java
@@ -0,0 +1,13 @@
+package com.SkbBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkbBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkbBankApplication.class, args);
+	}
+
+}
---
+++ SkbBank/src/test/java/com/SkbBank/SkbBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SkbBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
